a7e2c016f92d136843a3eeea7292132b9502d9d6
Aditi Valmiki <avalmiki6138@gmail.com>
2025-11-02 22:19:21 -0500

    fixed format and attempting to implement nav bar (via javascript)

--- src/contact_us/contact_us.js
+++ src/contact_us/contact_us.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     const targetElement = document.getElementById('navbar-container');
     if (targetElement){
-        fetch('nav_bar.html')
+        fetch('C:/Users/avalm/master-repo/nav_bar.html')
             .then(response =>{
                 if(!response.ok){
                     throw new Error(`HTTP error! status: ${response.status}`);
